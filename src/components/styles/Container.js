1fcf4d46f6cba6c21c46bb1a3c1effd11a714280
Shah <shahthepro@outlook.com>
2019-03-15 21:40:02 +0530

    Add Container component

---
+++ src/components/styles/Container.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 800px;
+  margin: 0 auto;
+  padding: 4rem 0;
+`;
+
+export default Container;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Container from './../components/styles/Container';
 
 class HomePage extends Component {
   render() {
     return (
-      <div>
+      <Container>
         Hello World
-      </div>
+      </Container>
     );
   }
 }
