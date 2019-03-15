9159c2d351af9782361b8a13e246262227570acb
Shah <shahthepro@outlook.com>
2019-03-15 22:11:24 +0530

    Minor style updates

--- src/components/styles/Container.js
+++ src/components/styles/Container.js
@@ -4,6 +4,7 @@ const Container = styled.div`
   width: 800px;
   margin: 0 auto;
   padding: 4rem 0;
+  box-sizing: border-box;
 `;
 
 export default Container;
--- src/components/styles/ListViewWrapper.js
+++ src/components/styles/ListViewWrapper.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const ListViewWrapper = styled.div`
   border: 1px solid #f0f0f0;
-  border-radius: 7px;
+  background-color: #fff;
 `;
 
 export default ListViewWrapper;
