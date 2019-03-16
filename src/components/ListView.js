c16679d54fdacf82b2d36bf91a3bcb7b3a226f8e
Shah <shahthepro@outlook.com>
2019-03-16 13:20:45 +0530

    Make ListView a stateless component

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { ListViewItem, ListViewItems, ListViewHeader, ListViewWrapper } from './styles/ListViewStyles';
 
-class ListView extends Component {
-  static propTypes = {
-    header: PropTypes.string,
-    items: PropTypes.array.isRequired
-  };
-
-  render() {
-    return (
-      <ListViewWrapper>
+const ListView = (props) => {
+  return (
+    <ListViewWrapper>
+      {
+        props.header && (
+          <ListViewHeader>
+            { props.header }
+          </ListViewHeader>
+        )
+      }
+      <ListViewItems className={ (!!props.header) ? 'hasHeader' : '' }>
         {
-          this.props.header && (
-            <ListViewHeader>
-              { this.props.header }
-            </ListViewHeader>
-          )
+          props.items.map(item => (
+            <ListViewItem key={item.id}>{ item.title }</ListViewItem>
+          ))
         }
-        <ListViewItems className={ (!!this.props.header) ? 'hasHeader' : '' }>
-          {
-            this.props.items.map(item => (
-              <ListViewItem key={item.id}>{ item.title }</ListViewItem>
-            ))
-          }
-        </ListViewItems>
-      </ListViewWrapper>
-    );
-  }
-}
+      </ListViewItems>
+    </ListViewWrapper>
+  );
+};
+
+ListView.propTypes = {
+  header: PropTypes.string,
+  items: PropTypes.array.isRequired
+};
 
 export default ListView;
